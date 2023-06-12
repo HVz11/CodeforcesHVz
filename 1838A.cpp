9c62fb7b71716eb7002b32e35dd4e14c1e0ed399
Vaibhav <vs630916@gmail.com>
2023-06-12 12:32:50 +0530

    Codeforces

---
+++ 1838A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &cin, vector<T> &v)
+{
+    for (T &e : v)
+        cin >> e;
+    return cin;
+}
+
+#define int long long
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vvi vector<vector<int>>
+#define vs vector<string>
+#define endl '\n'
+#define pb push_back
+#define pob pop_back()
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define mod 998244353
+
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void solve(){
+   int n;
+   cin >> n;
+   vi a(n);
+   for(int &x : a){
+    cin >> x;
+   }
+   sort(all(a));
+   if(a.front() < 0){
+    cout << a.front() << endl;
+   }
+   else{
+    cout << a.back() << endl;
+   }
+}
+
+int32_t main(){
+    fio;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
