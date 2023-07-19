facc611d0347b41f5a43c0098e2b02a2e312d305
Vaibhav <vs630916@gmail.com>
2023-07-19 21:03:42 +0530

    Codeforces

---
+++ 1805A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &cin, vector<T> &v){
+    for (T &e : v)
+        cin >> e;
+    return cin;
+}
+
+#define int long long
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define vvi vector<vector<int>>
+#define vs vector<string>
+#define endl '\n'
+#define pb push_back
+#define pob pop_back
+#define all(x) x.begin(), x.end()
+#define mod 998244353
+
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void solve(){
+    int n; 
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int xorValue = 0;
+    for (int i=0; i<n; i++){
+        xorValue ^= arr[i];
+    }
+
+    if (xorValue == 0){
+        cout << 0 << endl;
+    } 
+    else{
+        if (n % 2 == 1) {
+            cout << xorValue << endl;
+        } 
+        else{
+            cout << -1 << endl;
+        }
+    }
+}
+
+int32_t main(){
+    fio;
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
